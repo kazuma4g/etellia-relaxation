2ddcedffbbe8dc08263cf1ed0df6a56a498a8eda
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-10 12:33:05 -0400

    implement header

--- header.php
+++ header.php
@@ -10,4 +10,15 @@
             class="reservation_btn"
             onclick="window.location.href='index.php'">ご予約はこちら</button>
     </div>
+    <div class="header_menu_bar">
+        <nav>
+            <ul class="header_nav">
+                <li><a href="index.php">トップ</a></li>
+                <li><a href="#">サロンについて</a></li>
+                <li><a href="#">メニュー</a></li>
+                <li><a href="#">注意事項</a></li>
+                <li><a href="#">求人情報</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
