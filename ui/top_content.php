c6d5f8a80b142e671f0f3c8e2ea7d41681b8a727
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-11 10:40:11 -0400

    implement circle img link

--- ui/top_content.php
+++ ui/top_content.php
@@ -2,9 +2,18 @@
     <div class="page_top_img_parent">
         <div class="page_top_img_text">
             心から癒される時間と空間
-                <span>
-                    Location time and space that is healed from the bottom my heal.
-                </span>
+            <span> Location time and space that is healed from the bottom my heal. </span>
+        </div>
+    </div>
+    <div class="page_top_circle_img_parent">
+        <div class="circle_img_salon">
+            
+        </div>
+        <div class="circle_img_menu">
+            
+        </div>
+        <div class="circle_img_note">
+            
         </div>
     </div>
 </div>
