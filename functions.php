517a14e5eb86866b0f1d69c719f90eeff4d95e32
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-12 10:14:37 -0400

    changed directory

--- functions.php
+++ functions.php
@@ -15,38 +15,47 @@ function add_stylesheet(){
         'main_style', 
         get_stylesheet_directory_uri().'/style.css' 
     );
+    
+}
 
+function add_pc_stylesheet_files(){
+    wp_enqueue_style(
+        'header_style_pc',
+        get_stylesheet_directory_uri().'/css_pc/header_style.css'
+    );
+
+    wp_enqueue_style(
+        'top_page_content_style_pc',
+        get_stylesheet_directory_uri().'/css_pc/top_content_style.css'
+    );
+
+    wp_enqueue_style(
+        'footer_style_pc',
+        get_stylesheet_directory_uri().'/css_pc/footer_style.css'
+    );
+
+}
+
+function add_smart_phone_stylesheet_files(){
     wp_enqueue_style( 
         'smart_phone_main_style', 
-        get_stylesheet_directory_uri().'/css_main/style_smart_phone.css' 
-    );
-
-    wp_enqueue_style( 
-        'tablet_main_style', 
-        get_stylesheet_directory_uri().'/css_main/style_tablet.css' 
-    );
-
-    wp_enqueue_style(
-        'header_style',
-        get_stylesheet_directory_uri().'/css_header/header_style.css'
-    );
-
-    wp_enqueue_style(
-        'footer_style',
-        get_stylesheet_directory_uri().'/css_footer/footer_style.css'
-    );
-
-    wp_enqueue_style(
-        'top_page_content_style',
-        get_stylesheet_directory_uri().'/css_top/top_content_style.css'
+        get_stylesheet_directory_uri().'/css_smart_phone/style_smart_phone.css' 
     );
 
     wp_enqueue_style(
         'top_page_content_style_smart_phone',
-        get_stylesheet_directory_uri().'/css_top/top_content_style_smart_phone.css'
+        get_stylesheet_directory_uri().'/css_smart_phone/top_content_style_smart_phone.css'
     );
-    
 }
+
+function add_tablet_stylesheet_files(){
+    wp_enqueue_style( 
+        'tablet_main_style', 
+        get_stylesheet_directory_uri().'/css_tablet/style_tablet.css' 
+    );
+}
+
+
 
 function google_fonts() {
     wp_enqueue_style( 
@@ -58,6 +67,9 @@ function google_fonts() {
 
 add_action('wp_enqueue_scripts', 'add_reset_stylesheet');
 add_action('wp_enqueue_scripts', 'add_stylesheet');
+add_action('wp_enqueue_scripts', 'add_pc_stylesheet_files');
+add_action('wp_enqueue_scripts', 'add_smart_phone_stylesheet_files');
+add_action('wp_enqueue_scripts', 'add_tablet_stylesheet_files');
 add_action('wp_enqueue_scripts', 'google_fonts');
 
 ?>
