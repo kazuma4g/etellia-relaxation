f6ab7b4c98f73d000217a79562588019c8b200f7
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-12 02:06:27 -0400

    implement map

--- functions.php
+++ functions.php
@@ -1,10 +1,24 @@
 <?php
 
+function add_reset_stylesheet(){
+
+    wp_enqueue_style( 
+        'reset_style', 
+        get_stylesheet_directory_uri().'/css/reset_style.css' 
+    );
+    
+}
+
 function add_stylesheet(){
 
     wp_enqueue_style( 
         'main_style', 
         get_stylesheet_directory_uri().'/style.css' 
+    );
+
+    wp_enqueue_style( 
+        'smart_phone_main_style', 
+        get_stylesheet_directory_uri().'/css/style_smart_phone.css' 
     );
 
     wp_enqueue_style(
@@ -15,6 +29,11 @@ function add_stylesheet(){
     wp_enqueue_style(
         'top_page_content_style',
         get_stylesheet_directory_uri().'/css/top_content_style.css'
+    );
+
+    wp_enqueue_style(
+        'top_page_content_style_smart_phone',
+        get_stylesheet_directory_uri().'/css/top_content_style_smart_phone.css'
     );
     
 }
@@ -27,6 +46,7 @@ function google_fonts() {
     );
 }
 
+add_action('wp_enqueue_scripts', 'add_reset_stylesheet');
 add_action('wp_enqueue_scripts', 'add_stylesheet');
 add_action('wp_enqueue_scripts', 'google_fonts');
 
--- ui/top_content.php
+++ ui/top_content.php
@@ -74,6 +74,12 @@
                 </div>
                 <hr/>
             </div>
+            <iframe 
+                class="page_top_map"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12946.546667419523!2d139.9006607!3d35.7843004!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x69dd1692ac336e85!2z5p2-5oi46aeF!5e0!3m2!1sja!2sca!4v1660284016819!5m2!1sja!2sca" 
+                allowfullscreen="" 
+                loading="lazy" 
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </div>
