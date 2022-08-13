135ddbe8efc9f579d60bb31e4d5e4ce1632744b9
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-13 18:32:01 -0400

    hamburger menu

--- js/hamburger_menu.js
+++ js/hamburger_menu.js
@@ -1,7 +1,31 @@
 const hamburger =  document.querySelector('.hamburger');
 const navMenu =  document.querySelector('.nav_menu');
 
+var isActive = false;
+
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    if(isActive) {
+        enableScroll();
+        isActive = false;
+    }else {
+        disableScroll();
+        isActive = true;
+    }
 })
+
+function disableScroll() {
+    // Get the current page scroll position
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  
+        // if any scroll is attempted, set this to the previous value
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+  
+function enableScroll() {
+    window.onscroll = function() {};
+}
