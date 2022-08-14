1b8b4c6f88d97a0992a3671150830c563074e654
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-14 10:22:15 -0400

    implement about us

--- ui/about_content.php
+++ ui/about_content.php
@@ -14,7 +14,50 @@
             </div>
         </div>
         <div class="about_us_content_parent">
-            
+            <div class="about_us_content1_parent">
+                <div class="about_us_content1_img_parent">
+                    <div class="about_us_content1_img"></div>
+                </div>
+                <div class="about_us_content1_text_parent">
+                    <div class="about_us_content1_text">
+                        <p class="about_us_textblock1">JR松戸駅から徒歩1分</p>
+                        「残業の後に行きたい」<br>
+                        「疲れを明日に持ちこしたくない」<br>
+                        「身体も心も疲れちゃった···」<br>
+                        <p class="about_us_textblock2">
+                            <nobr>という方を少しでも癒したいという思いから誕生しました。</nobr>
+                            
+                        </p>
+                        <p class="about_us_textblock2">
+                            仕事帰り・残業帰りの時間でも<br>
+                            <nobr>気軽に通ってもらえるサロンになれば幸いです。</nobr>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="about_us_content2_parent">
+                <div class="about_us_content2_text_parent">
+                    <div class="about_us_content2_text">
+                        <p class="about_us_textblock1">
+                            日々の疲れ、不調は、全身のバランスの乱れが原因です。<br>
+                        </p>
+                        <p class="about_us_textblock1">
+                            ETELLIAリラクゼーションでは心身のバランスをとり、<br>
+                            当たり前の日々をありがたい日々に変えていく<br>
+                            リラクゼーションをご提供します。
+                        </p>
+                        <p class="about_us_textblock1">
+                            <nobr>
+                            こだわりのVIPルームには足湯やメイクスペースも完備しております。
+                            </nobr>
+                        </p>
+                        <p>極上の癒しの時間をお過ごしください♪</p>
+                    </div>
+                </div>
+                <div class="about_us_content2_img_parent">
+                    <div class="about_us_content2_img"></div>
+                </div>
+            </div>
         </div>
     </div>
     
