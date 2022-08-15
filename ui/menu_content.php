07ff7614b9508236c0bb0f5a2b7e721501893753
kazuma4g <93083318+kazuma4g@users.noreply.github.com>
2022-08-14 21:16:30 -0400

    implement facial

--- ui/menu_content.php
+++ ui/menu_content.php
@@ -19,5 +19,27 @@
                 お肌の細菌を除去し、高濃度ヒト幹細胞美容液より浸透させる事でうるツヤ肌に♪
             </div>
         </div>
+        <div class="facial_menu_list_parent">
+            <div class="smaller_facial_parent">
+                <div class="smaller_facial_title">小顔フェイシャル</div>
+                <div class="smaller_facial_price_list_parent">
+                    <div class="smaller_facial_price1">
+                        60分<span>¥3,000</span>
+                    </div>
+                    <div class="smaller_facial_price2">
+                        90分<span>¥5,000</span>
+                    </div>
+                    <div class="smaller_facial_price3">
+                        120分<span>¥7,000</span>
+                    </div>
+                </div>
+            </div>
+            <div class="smaller_adjustment_parent">
+                
+            </div>
+            <div class="prasma_shower_parent">
+                
+            </div>
+        </div>
     </div>
 </div>
